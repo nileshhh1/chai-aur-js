c015cb8c29bb31bd406cd1e366c787662d80a781
nileshhh1 <159626645+nileshhh1@users.noreply.github.com>
2024-07-19 18:49:56 +0000

    datatypes

---
+++ 01_basics/01-variable.js
@@ -0,0 +1,6 @@
+const accountID=123455;
+let accountMail="nileshhh123@gmail.com";
+var accountPass="1212212";
+accountCity="nooida";
+
+console.table([accountID,accountMail,accountPass,accountCity]);
---
+++ 01_basics/2datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as new version
+
+console.log(3+3);
+
+let age=9;
+let isLoggedIn=false
+let state= null // khali h
+
+// number => 2 to the power 53
+// bigint => use to store a very large number
+// string => used to store multiple characters within single quote ' ', double qoute " ", and backtik ` `;
+// undefined=> variable is declared but value is not initialised
+// eg let a; output is undefined
+// symbol=> unique
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
