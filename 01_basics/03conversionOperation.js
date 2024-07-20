3a58678eb74eb901d133cd99862ac1d0800d71ed
nileshhh1 <159626645+nileshhh1@users.noreply.github.com>
2024-07-20 04:55:50 +0000

    4 done

--- 01_basics/03conversionOperation.js
+++ 01_basics/03conversionOperation.js
@@ -2,5 +2,24 @@
  console.log(typeof score);
  console.log(score);
 
- let value=Number(score)
- console.log(typeof value);
+//  let value=Number(score)
+//  console.log(typeof value);
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// some operations are
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); //two to the power of 3
+console.log(2/3);
+console.log(2%3); */
+
+
+let str1="hello"
+let str2="nileshh"
+let str3=str1+str2
+console.log(str3);
+
---
+++ 01_basics/4comparison.js
@@ -0,0 +1,20 @@
+// js will automatically change string to number and perfrom the comparison operation
+
+console.log("2">1);
+console.log("02">1);
+console.log(2===2); //this === comparison operator will strictly check if the datatype and value are same.
+console.log(null>0);
+console.log(null==0);
+
+
+// on comparing null with >= or <= then the output will be true and for other cases o/p will be false
+
+console.log(null>=0);
+console.log(null<=0);
+
+// for undefined all comaprison are false
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+console.log(undefined<0);
+console.log(undefined>0);
